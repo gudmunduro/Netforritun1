4a714215eef9dc278b7c3d75a1abda1ce3ec6ded
Helgi Steinarr <HelgiSteinarr@users.noreply.github.com>
2019-01-30 14:44:59 +0000

    Add files via upload

--- Client/verk1/Program.cs
+++ Client/verk1/Program.cs
@@ -5,47 +5,83 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.NetworkInformation;
 
 namespace verk1
 {
     class Program
     {
         static TcpClient tcpClient;
+        static NetworkStream networkStream;
         static void Main(string[] args)
         {
             ConnectToServer();
-            Console.WriteLine("main 2");
-            while (true) ;
+            while (true) WhileConnected();
+        }
+
+        static void WhileConnected()
+        {
+            string command = Console.ReadLine();
+            byte[] writeBuffer = Encoding.UTF8.GetBytes(command);
+            networkStream.Write(writeBuffer, 0, writeBuffer.Length);
+
+            if (command == "") return;
+            while (true)
+            {
+                bool state = tcpClient.Client.Connected;
+                if (!state)
+                {
+                    Console.WriteLine("con closed");
+                }
+                if (networkStream.DataAvailable)
+                {
+
+                    try
+                    {
+
+                        byte[] bytes = new byte[1024];
+                        networkStream.Read(bytes, 0, 1024);
+                        
+                        string data = Encoding.UTF8.GetString(bytes);
+                        Console.WriteLine("Server response:" + System.Environment.NewLine + "{0}", data);
+                        return;
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("No message received");                               
+                    }
+                }
+                
+                
+            }
         }
 
         static void ConnectToServer()
         {
-            Console.WriteLine("byrjun connect 2 server()");
+            Console.WriteLine("Start of connect function");
 
             tcpClient = new TcpClient();
 
             //IPAddress ipAddress = Dns.GetHostEntry("tsuts.tskoli.is/2t/0812003270").AddressList[0];
             IPAddress ipAddress = IPAddress.Parse("10.220.226.70");
-            tcpClient.Connect(ipAddress, 1103);
-
-            Console.WriteLine("NW STREAM");
-
-            NetworkStream networkStream = tcpClient.GetStream();
-            networkStream.ReadTimeout = 5000; // Mili secs
-
+            try
+            {
+                tcpClient.Connect(ipAddress, 1103);
+                networkStream = tcpClient.GetStream();
+                networkStream.ReadTimeout = 5000; // Mili secs
+                Console.WriteLine("Connected to " + ipAddress.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect: {0}", ex.ToString());
+            }
             
 
-            byte[] bytes = new byte[1024];
-            
-            Console.WriteLine("NW stream read");
-
-            networkStream.Read(bytes, 0, 1024);
-            string data = Encoding.UTF8.GetString(bytes);
-            System.Threading.Thread.Sleep(5000);
-            Console.WriteLine("Server sent message: {0}", data);
             //networkStream.Close();
             //tcpClient.Close();
-            return;
         }
+
     }
+
 }
